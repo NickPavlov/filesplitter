8d557265d3dad8385ffbd3087e651f59e9b49293
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-10 09:59:06 +0300

    The IDataFinder interface. Defines the behavior of the data finder.

--- src/main/java/com/sysgears/filesplitter/model/abstractmodel/IData.java
+++ src/main/java/com/sysgears/filesplitter/model/abstractmodel/IData.java
@@ -3,7 +3,7 @@ package com.sysgears.filesplitter.model.abstractmodel;
 import java.net.URI;
 
 /**
- * The IData interface defines the behavior of a data object.
+ * The IData interface defines the behavior of the data storage object.
  */
 public interface IData {
 
---
+++ src/main/java/com/sysgears/filesplitter/model/abstractmodel/IDataFinder.java
@@ -0,0 +1,15 @@
+package com.sysgears.filesplitter.model.abstractmodel;
+
+/**
+ * The IDataFinder interface defines the behavior of the data finder.
+ */
+public interface IDataFinder {
+
+    /**
+     * Gets data by name.
+     *
+     * @param name data name
+     * @return data object
+     */
+    public IData getByName(final String name);
+}
