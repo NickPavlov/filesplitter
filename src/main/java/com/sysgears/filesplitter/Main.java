16d5d20b7917377e74904c60c03e6cc5c68b42e5
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-10 12:40:02 +0300

    The WorkerFactory class. Provides functionality to create workers.

--- src/main/java/com/sysgears/filesplitter/Main.java
+++ src/main/java/com/sysgears/filesplitter/Main.java
@@ -2,6 +2,9 @@ package com.sysgears.filesplitter;
 
 import com.sysgears.filesplitter.view.UserInterface;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * The Main class creates the necessary objects for calculator work and passes control to the service.
  */
@@ -14,7 +17,10 @@ public class Main {
      */
     public static void main(final String[] args) {
         try {
-            new Service(new UserInterface(System.in, System.out)).start();
+            final ExecutorService pool = Executors.newFixedThreadPool(2);
+            final UserInterface ui = new UserInterface(System.in, System.out);
+
+            new Service(pool, ui).start();
         } catch (Throwable t) {
             t.printStackTrace();
         }
---
+++ src/main/java/com/sysgears/filesplitter/model/WorkerFactory.java
@@ -0,0 +1,18 @@
+package com.sysgears.filesplitter.model;
+
+import com.sysgears.filesplitter.model.abstractmodel.Worker;
+
+/**
+ * The WorkerFactory class provides functionality to create workers.
+ */
+public class WorkerFactory {
+
+    /**
+     * Creates new worker object.
+     *
+     * @return worker
+     */
+    public static Worker newWorker() {
+        return null;
+    }
+}
