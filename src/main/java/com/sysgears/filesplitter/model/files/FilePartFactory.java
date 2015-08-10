c78fae5a05df57d75c6bde9547f7504c1113ab2f
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-10 12:52:06 +0300

    FilePartFactory - constructor new params: original file, file part creator.

--- src/main/java/com/sysgears/filesplitter/model/files/FilePartFactory.java
+++ src/main/java/com/sysgears/filesplitter/model/files/FilePartFactory.java
@@ -1,11 +1,23 @@
 package com.sysgears.filesplitter.model.files;
 
+import com.sysgears.filesplitter.model.abstractmodel.IData;
+import com.sysgears.filesplitter.model.abstractmodel.IDataProcessor;
 import com.sysgears.filesplitter.model.abstractmodel.Worker;
 
 /**
  * The FilePartFactory class provides functionality to create workers.
  */
 public class FilePartFactory {
+
+    /**
+     * Original file.
+     */
+    private final IData originalFile;
+
+    /**
+     * Part creator.
+     */
+    private final IDataProcessor partCreator;
 
     /**
      * Part size.
@@ -22,7 +34,9 @@ public class FilePartFactory {
      *
      * @param partSize part size
      */
-    public FilePartFactory(final long partSize) {
+    public FilePartFactory(final IData originalFile, final IDataProcessor partCreator, final long partSize) {
+        this.originalFile = originalFile;
+        this.partCreator = partCreator;
         this.partSize = partSize;
     }
 
