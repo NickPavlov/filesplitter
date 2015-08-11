07165275e2a679ef058bea51193b8a924cb3cb11
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-11 13:09:22 +0300

    comments correction

--- src/main/java/com/sysgears/filesplitter/model/file/part/PartCreator.java
+++ src/main/java/com/sysgears/filesplitter/model/file/part/PartCreator.java
@@ -44,7 +44,7 @@ public class PartCreator implements IDataProcessor {
      * Creates the PartCreator object specified by part name and size.
      *
      * @param partNumber      file part number
-     * @param partSize        final part size
+     * @param partSize        part size
      * @param outputDirectory output directory
      */
     public PartCreator(final int partNumber, final long partSize, final String outputDirectory) {
