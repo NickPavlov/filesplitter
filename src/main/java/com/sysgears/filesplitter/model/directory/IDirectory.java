255f6de35f15cee75c798ff0f27836fe3c0c1d29
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-12 11:43:10 +0300

    The new "directory.IDirectory" interface. Defines the behavior of a directory object.

---
+++ src/main/java/com/sysgears/filesplitter/model/directory/IDirectory.java
@@ -0,0 +1,41 @@
+package com.sysgears.filesplitter.model.directory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * The IDirectory interface defines the behavior of a directory object.
+ */
+public interface IDirectory {
+
+    /**
+     * Returns an absolute path with an inner directory.
+     * The original directory is not changed.
+     *
+     * @param innerDirectory inner directory name
+     * @return an absolute path
+     */
+    public String appendInnerDirectory(final String innerDirectory) throws IOException;
+
+    /**
+     * Returns a directory.
+     *
+     * @return directory
+     */
+    public File getDirectory();
+
+    /**
+     * Returns a directory absolute path.
+     *
+     * @return absolute path
+     */
+    public String getAbsolutePath();
+
+    /**
+     * Returns a <code>Path</code> of the directory.
+     *
+     * @return <code>Path</code>
+     */
+    public Path getPath();
+}
