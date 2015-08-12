06cf46dc050cc36eda0f09c8265f13f7a8601feb
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-12 18:31:03 +0300

    Spring: xml based.

--- src/main/java/com/sysgears/filesplitter/Main.java
+++ src/main/java/com/sysgears/filesplitter/Main.java
@@ -2,7 +2,7 @@ package com.sysgears.filesplitter;
 
 import com.sysgears.filesplitter.controller.FileSplitController;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * The Main class creates the necessary objects for calculator work and passes control to the service.
@@ -16,7 +16,9 @@ public class Main {
      */
     public static void main(final String[] args) {
         try {
-            final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+            //final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+            final ApplicationContext context
+                    = new ClassPathXmlApplicationContext("controllers.xml", "view.xml");
 
             final FileSplitController splitController = context.getBean(FileSplitController.class);
 
