7f8faa7c0259f6cd9fd96aefa57d2205c58f2548
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-12 11:08:46 +0300

    "directory.Directory": added set of getters.

--- src/main/java/com/sysgears/filesplitter/model/directory/Directory.java
+++ src/main/java/com/sysgears/filesplitter/model/directory/Directory.java
@@ -2,6 +2,8 @@ package com.sysgears.filesplitter.model.directory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * The Directory class provides functionality to work with directories.
@@ -14,25 +16,65 @@ public class Directory {
     private final File directory;
 
     /**
-     * Creates the Directory instance.
+     * Creates the Directory instance specified by directory file.
+     * Path can be specified for a particular file, in this case
+     * the constructor takes the parent directory of the file
      *
-     * @param directory directory
-     * @throws IllegalArgumentException in case if <code>directory</code> is null
-     * @throws IOException
+     * @param directory directory path
+     * @throws IllegalArgumentException if <code>directory</code> is null
+     * @throws IOException              if an I/O error
      */
-    public Directory(final File directory) throws IOException {
+    public Directory(final String directory) throws IOException {
         if (directory == null) {
             throw new IllegalArgumentException("Directory can't be null.");
         }
-        if (directory.exists()) {
-            this.directory = directory.isDirectory() ? directory : directory.getParentFile();
-        } else {
-            if (directory.mkdir()) {
-                this.directory = directory;
-            } else {
-                throw new IOException("Error creating directory.");
-            }
+        if (directory.isEmpty()) {
+            throw new IllegalArgumentException("Directory can't be empty.");
         }
 
+        File tempDirectory = new File(directory);
+        if (tempDirectory.exists() && !tempDirectory.isDirectory()) {
+            tempDirectory = tempDirectory.getParentFile();
+        } else if (!tempDirectory.mkdir()) {
+            throw new IOException("Error creating directory.");
+        }
+        this.directory = tempDirectory;
+    }
+
+    /**
+     * Returns an absolute path with inner directory.
+     *
+     * @param innerDirectory inner directory name
+     * @return an absolute path
+     */
+    public String getWithInnerDirectory(final String innerDirectory) {
+        return "";
+    }
+
+    /**
+     * Returns a directory.
+     *
+     * @return directory
+     */
+    public File getDirectory() {
+        return directory;
+    }
+
+    /**
+     * Returns a directory absolute path.
+     *
+     * @return absolute path
+     */
+    public String getAbsolutePath() {
+        return directory.getAbsolutePath();
+    }
+
+    /**
+     * Returns a <code>Path</code> of the directory.
+     *
+     * @return <code>Path</code>
+     */
+    public Path getPath() {
+        return Paths.get(directory.toURI());
     }
 }
