278225e591c21088a5af10b478a04a9f4f50fac6
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-12 15:27:39 +0300

    New ProgressMonitor class(without implementation). Provides functionality to gather information about the work done.

---
+++ src/main/java/com/sysgears/filesplitter/model/statistics/ProgressMonitor.java
@@ -0,0 +1,7 @@
+package com.sysgears.filesplitter.model.statistics;
+
+/**
+ * The ProgressMonitor class provides functionality to gather information about the work done.
+ */
+public class ProgressMonitor {
+}
