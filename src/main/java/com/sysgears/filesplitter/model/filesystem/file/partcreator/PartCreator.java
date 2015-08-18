f03d736673b4cb6ea48a2e886d7e3196216f73f2
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-18 15:40:34 +0300

    PartCreator: code refactoring.

--- src/main/java/com/sysgears/filesplitter/model/filesystem/file/partcreator/PartCreator.java
+++ src/main/java/com/sysgears/filesplitter/model/filesystem/file/partcreator/PartCreator.java
@@ -92,11 +92,8 @@ public class PartCreator implements IDataProcessor {
                 final FileChannel inputChannel = ((FileChannel) originalFile.getChannel()).position(position);
                 final FileChannel outputChannel = outputFile.getChannel();
         ) {
-            //outputChannel.write(ByteBufferCreator.createFromString(originalFile.getName(), 4));
             outputChannel.write(ByteBufferCreator.createFromLong(position));
-
             IPartIterator partIterator = new PartIterator(partSize, DEFAULT_BUFFER_SIZE);
-
             ByteBuffer buffer;
             long readBytes = 0;
             final FileLock lock = inputChannel.lock(position, partSize, false);
