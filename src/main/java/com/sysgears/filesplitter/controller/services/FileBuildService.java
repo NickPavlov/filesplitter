bbf9ac4d044cb032423bbb954033bdf4f2f4d024
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-18 16:46:28 +0300

    FileBuildService: now uses WorkersFactory to create workers.

--- src/main/java/com/sysgears/filesplitter/controller/services/FileBuildService.java
+++ src/main/java/com/sysgears/filesplitter/controller/services/FileBuildService.java
@@ -2,12 +2,12 @@ package com.sysgears.filesplitter.controller.services;
 
 import com.sysgears.filesplitter.model.abstractmodel.IData;
 import com.sysgears.filesplitter.model.abstractmodel.IDataIterator;
-import com.sysgears.filesplitter.model.abstractmodel.Worker;
 import com.sysgears.filesplitter.model.consoleoptions.BuildOptions;
 import com.sysgears.filesplitter.model.filesystem.file.FileData;
 import com.sysgears.filesplitter.model.filesystem.file.FileFinder;
 import com.sysgears.filesplitter.model.filesystem.file.filebuilder.FileBuilder;
 import com.sysgears.filesplitter.model.statistics.monitor.IProgressMonitor;
+import com.sysgears.filesplitter.model.workers.WorkersFactory;
 import com.sysgears.filesplitter.view.IUserInterface;
 
 import java.io.File;
@@ -64,10 +64,12 @@ public class FileBuildService implements Runnable {
      */
     public void run() {
         try {
-            String partPath = "/home/nick/Documents/jdk.tar.gz_parts/jdk.tar.gz_part0.bin";
-            String outputPath = "/home/nick/Documents/jdk.tar.gz_parts/";
 
+            // temporarily
+            final String partPath = "/home/nick/Documents/jdk.tar.gz_parts/jdk.tar.gz_part0.bin";
+            final String outputPath = "/home/nick/Documents/jdk.tar.gz_parts/";
             final String regexPattern = ".+(?=_part[0-9]+\\.bin)";
+
             final Pattern pattern = Pattern.compile(regexPattern);
             String originalFileName = Long.toString(System.nanoTime());
             Matcher matcher = pattern.matcher(new File(partPath).getName());
@@ -78,13 +80,12 @@ public class FileBuildService implements Runnable {
 
             IDataIterator fileIterator = new FileFinder(partPath).iterator();
             FileData fileData = new FileData(new File(outputPath + originalFileName));
-
+            WorkersFactory factory = new WorkersFactory();
             IData filePart;
-            int i = 0;
             while (fileIterator.hasNext()) {
                 filePart = fileIterator.next();
                 if (pattern.matcher(filePart.getName()).find()) {
-                    pool.execute(new Worker("worker-" + i++, filePart, new FileBuilder(fileData, progressMonitor)));
+                    pool.execute(factory.create(filePart, new FileBuilder(fileData, progressMonitor)));
                 }
             }
             pool.shutdown();
