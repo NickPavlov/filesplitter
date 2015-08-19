2f0b26e4ec5db0b9098eaefe0a21290ac2028697
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-19 14:28:45 +0300

    New interface IService. Defines the behavior of a service object.

---
+++ src/main/java/com/sysgears/filesplitter/controller/services/IService.java
@@ -0,0 +1,17 @@
+package com.sysgears.filesplitter.controller.services;
+
+/**
+ * The IService interface defines the behavior of a service object.
+ */
+public interface IService extends Runnable {
+
+    /**
+     * Starts a service.
+     */
+    public void start();
+
+    /**
+     * Stops a service.
+     */
+    public void stop();
+}
