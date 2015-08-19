41b212ea961c885a3b438cb71523931af18669d3
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-19 16:27:07 +0300

    Main: logger added.

--- src/main/java/com/sysgears/filesplitter/Main.java
+++ src/main/java/com/sysgears/filesplitter/Main.java
@@ -1,12 +1,18 @@
 package com.sysgears.filesplitter;
 
 import com.sysgears.filesplitter.controller.MainController;
+import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * The Main class. Configures and starts the application.
  */
 public class Main {
+
+    /**
+     * Logger.
+     */
+    private final static Logger LOG = Logger.getLogger(Main.class);
 
     /**
      * Application context paths.
@@ -28,6 +34,7 @@ public class Main {
             context.getBean(MainController.class).start(args);
 
         } catch (Throwable t) {
+            LOG.fatal(t.getMessage());
             t.printStackTrace();
         }
     }
