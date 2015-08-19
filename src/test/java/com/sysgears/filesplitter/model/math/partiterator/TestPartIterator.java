7f2e7d1369d2fcd0d345b027069830d103158822
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-19 11:28:04 +0300

    TestPartIterator class. Provides set of unit tests to test PartIterator.

---
+++ src/test/java/com/sysgears/filesplitter/model/math/partiterator/TestPartIterator.java
@@ -0,0 +1,29 @@
+package com.sysgears.filesplitter.model.math.partiterator;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * The TestPartIterator class provides set of unit tests to test PartIterator.
+ */
+public class TestPartIterator {
+
+    @BeforeClass
+    public void beforeClass() {
+    }
+
+    @Test
+    public void hasNextTest() {
+
+    }
+
+    @Test
+    public void nextTest() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+    }
+}
