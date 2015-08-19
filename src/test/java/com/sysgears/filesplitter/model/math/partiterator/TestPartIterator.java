2dc5350957ad1b9b4b00a814bb1aba298c9966b3
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-19 11:44:40 +0300

    TestPartIterator: now tests hasNext() and next() methods.

--- src/test/java/com/sysgears/filesplitter/model/math/partiterator/TestPartIterator.java
+++ src/test/java/com/sysgears/filesplitter/model/math/partiterator/TestPartIterator.java
@@ -1,5 +1,6 @@
 package com.sysgears.filesplitter.model.math.partiterator;
 
+import junit.framework.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -9,18 +10,34 @@ import org.testng.annotations.Test;
  */
 public class TestPartIterator {
 
+    /**
+     * Total interval.
+     */
+    private final int totalSize = 10;
+
+    /**
+     * Part size.
+     */
+    private final int partSize = 10;
+
+    /**
+     * PartIterator instance.
+     */
+    private final PartIterator partIterator = new PartIterator(totalSize, partSize);
+
+
     @BeforeClass
     public void beforeClass() {
     }
 
     @Test
     public void hasNextTest() {
-
+        Assert.assertEquals(partIterator.hasNext(), true);
     }
 
     @Test
     public void nextTest() {
-
+        Assert.assertEquals(partIterator.next(), 10);
     }
 
     @AfterClass
