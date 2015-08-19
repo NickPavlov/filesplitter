e43b4fc1b949dbdc7d619923ce3e63680b1d27e0
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-19 12:33:56 +0300

    New TestProgressState class. Provides set of unit tests to test ProgressState.

---
+++ src/test/java/com/sysgears/filesplitter/model/statistics/state/TestProgressState.java
@@ -0,0 +1,27 @@
+package com.sysgears.filesplitter.model.statistics.state;
+
+import junit.framework.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * The TestProgressState class provides set of unit tests to test ProgressState.
+ */
+public class TestProgressState {
+
+    @BeforeClass
+    public void init() {
+        System.out.println("ProgressState test starts.");
+    }
+
+    @AfterClass
+    public void tearDown() {
+    }
+
+    @Test
+    public void toString50PercentTest() {
+        ProgressState progressState = new ProgressState(50, 100);
+        Assert.assertEquals(progressState.toString(), "50%");
+    }
+}
